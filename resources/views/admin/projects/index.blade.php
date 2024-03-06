10bf3ce75ad32169c06bc67f4c621877001e6eb2
Richiisontheway <mineiriccardo@gmail.com>
2024-03-06 16:39:58 +0100

    finish index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,11 +8,42 @@
             <div class="card">
                 <div class="card-body">
                     <h1 class="text-center text-success">
-                        tutti i project
+                        Tutti i Project
                     </h1>
                     <br>
-                    La dashboard è una pagina privata (protetta dal middleware)
                 </div>
+                
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Titolo</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Azioni</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($project as $item)
+                                <tr>
+                                    <th scope="row">{{$item->id}}</th>
+                                    <td>{{$item->title}}</td>
+                                    <td>{{$item->date}}</td>
+                                    <td>
+                                        <a href="" class="btn btn-primary">
+                                            Show
+                                        </a>
+                                        <a href="" class="btn btn-warning">
+                                            Edit
+                                        </a>
+                                        <a href="" class="btn btn-danger">
+                                            Delete
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
             </div>
         </div>
     </div>
